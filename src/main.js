be8dd70d9d45f0d2e19f9d85d7a1556b0867dbd9
鲁伟明 <bthqh2yxtj@privaterelay.appleid.com>
2020-12-09 16:29:16 +0800

    ss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import Vue from 'vue';
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 createApp(App)
   .use(store)
